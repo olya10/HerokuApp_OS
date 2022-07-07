9334aa5ac28b5d385a609ca9d7432c838e09e3c4
Olya <vsauchyk@scnsoft.com>
2022-07-07 17:18:51 +0300

    Commit Selectors CSS.txt

--- SelectorCSS.java
+++ SelectorCSS.java
@@ -38,27 +38,25 @@ public class SelectorCSS {
      public void CheckboxesTest() throws InterruptedException {
 
           WebElement CheckBox1 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)"));
-          Thread.sleep(2000);
 
           System.out.println("The checkbox selection state is - " + CheckBox1.isSelected());
 
           Assert .assertFalse(driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).isSelected());
 
           CheckBox1.click();
-          Thread.sleep(2000);
 
           Assert.assertTrue(driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).isSelected());
 
 
           WebElement CheckBox2 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
-          Thread.sleep(2000);
+
           System.out.println("The checkbox selection state is - " + CheckBox2.isSelected());
 
           Assert .assertTrue(driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).isSelected());
 
           CheckBox2.click();
           Assert .assertFalse(driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).isSelected());
-          Thread .sleep(2000);
+
 
 
      }
